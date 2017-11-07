8573b7fd9aad3ea1ba45d030f76c16757624d3da
luciorq <luciorqueiroz@gmail.com>
2017-11-07 08:50:51 +0000

    Plotting ready

--- server.R
+++ server.R
@@ -1,19 +1,12 @@
 #
-# This is the server logic of a Shiny web application. You can run the 
-# application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-# 
-#    http://shiny.rstudio.com/
-#
-
+# This is the server logic of a Shiny web application.
 library(shiny)
+#library(xlsx)
 library(tidyverse)
-library(readxl)
-library(scales)
-library(ggbeeswarm)
-library(stringr)
-
+#library(readxl)
+#library(scales)
+#library(stringr)
+#library(ggpubr)
 
 # Define server logic required by the app
 server <- function(input, output, session) {
@@ -25,7 +18,7 @@ server <- function(input, output, session) {
     ext <- tools::file_ext(inFile$name)
     file.rename(inFile$datapath,
                 paste(inFile$datapath, ext, sep="."))
-    df <- read_excel(paste(inFile$datapath, ext, sep="."), sheet="Results")
+    df <- readxl::read_excel(paste(inFile$datapath, ext, sep="."), sheet="Results")
     for (i in 1:nrow(df)){
       if(is.na(df[i,1])){ 
         next 
@@ -34,7 +27,7 @@ server <- function(input, output, session) {
         skip_num <- i
       }
     }
-    df1 <- read_excel(paste(inFile$datapath, ext, sep="."), sheet="Results", skip=skip_num)
+    df1 <- readxl::read_excel(paste(inFile$datapath, ext, sep="."), sheet="Results", skip=skip_num)
     for(i in 1:length(names(df1))){
       if (names(df1)[i] == "Cт"){
         names(df1)[i] <- "CT"
@@ -42,7 +35,7 @@ server <- function(input, output, session) {
     }
     df1
   })
-  #This function is repsonsible for loading in the second selected file
+  #This function is responsible for loading in the second selected file
   filedata2 <- reactive({
     inFile2 <- input$file2
     if (is.null(inFile2))
@@ -50,7 +43,7 @@ server <- function(input, output, session) {
     ext <- tools::file_ext(inFile2$name)
     file.rename(inFile2$datapath,
                 paste(inFile2$datapath, ext, sep="."))
-    df <- read_excel(paste(inFile2$datapath, ext, sep="."), sheet="Results")
+    df <- readxl::read_excel(paste(inFile2$datapath, ext, sep="."), sheet="Results")
     for (i in 1:nrow(df)){
       if(is.na(df[i,1])){ 
         next 
@@ -59,13 +52,24 @@ server <- function(input, output, session) {
         skip_num <- i
       }
     }
-    df2 <- read_excel(paste(inFile2$datapath, ext, sep="."), sheet="Results", skip=skip_num)
+    df2 <- readxl::read_excel(paste(inFile2$datapath, ext, sep="."), sheet="Results", skip=skip_num)
     for(i in 1:length(names(df2))){
       if (names(df2)[i] == "Cт"){
         names(df2)[i] <- "CT"
       }
     }
     df2
+  })
+  #This function is responsible for loading in the metadata file
+  metadata_table <- reactive({
+    inFile3 <- input$file_metadata
+    if (is.null(inFile3))
+      return(NULL)
+    ext <- tools::file_ext(inFile3$name)
+    file.rename(inFile3$datapath,
+                paste(inFile3$datapath, ext, sep="."))
+    df_metadata <- readxl::read_excel(paste(inFile3$datapath, ext, sep="."))
+    df_metadata
   })
   # The following set of functions populate the gene list selectors
   GeneList <- reactive({
@@ -113,18 +117,18 @@ server <- function(input, output, session) {
     
     excel_table <- excel_table[complete.cases(excel_table),]
     
-    for( i in 1:length(excel_table$`Sample Name`)){
-      excel_table$`Sample Name`[i] <- gsub("Sample ", "", excel_table$`Sample Name`[i])
-      excel_table$`Sample Name`[i] <- gsub("sample ", "", excel_table$`Sample Name`[i])
-      excel_table$`Sample Name`[i] <- gsub("sample", "", excel_table$`Sample Name`[i])
-      excel_table$`Sample Name`[i] <- gsub("Sample", "", excel_table$`Sample Name`[i])
-      if ( nchar(excel_table$`Sample Name`[i]) == 1 ){
-        excel_table$`Sample Name`[i] <- paste0("0", excel_table$`Sample Name`[i])
-      }
-    }
+   # for( i in 1:length(excel_table$`Sample Name`)){
+   #   excel_table$`Sample Name`[i] <- gsub("Sample ", "", excel_table$`Sample Name`[i])
+   #   excel_table$`Sample Name`[i] <- gsub("sample ", "", excel_table$`Sample Name`[i])
+   #   excel_table$`Sample Name`[i] <- gsub("sample", "", excel_table$`Sample Name`[i])
+   #   excel_table$`Sample Name`[i] <- gsub("Sample", "", excel_table$`Sample Name`[i])
+   #   if ( nchar(excel_table$`Sample Name`[i]) == 1 ){
+   #     excel_table$`Sample Name`[i] <- paste0("0", excel_table$`Sample Name`[i])
+   #   }
+   # }
     table <- data_frame(name = excel_table$`Target Name`,
                         value = excel_table$CT , 
-                        sample = paste(excel_table$`Sample Name`,excel_table$`Target Name`, sep = "-"),
+                        sample = excel_table$`Sample Name`,
                         Tm = excel_table$Tm1)
     table <- table %>% mutate(value = replace(value,value=="Undetermined",NA)) %>%
       mutate(value=as.numeric(value))
@@ -141,10 +145,14 @@ server <- function(input, output, session) {
     gene1_df$value[gene1_df$Tm > my_range1_upper()] <- NA
     gene1_df$value[gene1_df$Tm < my_range1_lower()] <- NA
     table <- gene1_df
-    Mean_Values = tapply(as.numeric(table$value), table$sample, mean, na.rm=TRUE)
-    table = data_frame(Sample = str_replace(names(Mean_Values),".*-",""),
-                       value = as.numeric(Mean_Values),
-                       sample_number = as.character(names(Mean_Values)))
+    mean_Ct_vector <- tapply(as.numeric(table$value), table$sample, mean, na.rm=TRUE)
+    tm_min_vector <- tapply(as.numeric(table$Tm), table$sample, min, na.rm = TRUE)
+    tm_max_vector <- tapply(as.numeric(table$Tm), table$sample, max, na.rm = TRUE)
+    tm_range_vector <- paste(round(tm_min_vector,digits = 1),"-",round(tm_max_vector, digits = 1))
+    table = data_frame(Sample = Gene1(),
+                       sample_ID = as.character(names(mean_Ct_vector)),
+                       value = as.numeric(mean_Ct_vector),
+                       Tm_range = tm_range_vector)
     table <-  table %>% arrange(Sample)
     table
   })
@@ -159,10 +167,14 @@ server <- function(input, output, session) {
     gene2_df$value[gene2_df$Tm > my_range2_upper()] <- NA
     gene2_df$value[gene2_df$Tm < my_range2_lower()] <- NA
     table <- gene2_df
-    Mean_Values = tapply(as.numeric(table$value), table$sample, mean, na.rm=TRUE)
-    table = data_frame(Sample = str_replace(names(Mean_Values),".*-",""),
-                       value = as.numeric(Mean_Values),
-                       sample_number = as.character(names(Mean_Values)))
+    mean_Ct_vector <- tapply(as.numeric(table$value), table$sample, mean, na.rm=TRUE)
+    tm_min_vector <- tapply(as.numeric(table$Tm), table$sample, min, na.rm = TRUE)
+    tm_max_vector <- tapply(as.numeric(table$Tm), table$sample, max, na.rm = TRUE)
+    tm_range_vector <- paste(round(tm_min_vector,digits = 1),"-",round(tm_max_vector, digits = 1))
+    table = data_frame(Sample = Gene2(),
+                       sample_ID = as.character(names(mean_Ct_vector)),
+                       value = as.numeric(mean_Ct_vector),
+                       Tm_range = tm_range_vector)
     table <-  table %>% arrange(Sample)
     table
   })
@@ -182,7 +194,7 @@ server <- function(input, output, session) {
         deltaCT_vector <- c(deltaCT_vector, deltaCT)
       }
     }
-    deltaCT_df <- target_df[,c(1,3)]
+    deltaCT_df <- target_df[,c(1,2)]
     deltaCT_df <- deltaCT_df %>% mutate("deltaCT" = as.numeric(deltaCT_vector))
     deltaCT_df
   })
@@ -191,66 +203,31 @@ server <- function(input, output, session) {
     deltaCtTable()
   },digits = -2)
   
-  # This function creates the sliders for group subsetting
-  RowNumbers <- reactive({
-    df <- deltaCtTable()
-    nrow(df)
-  })
-  output$group_sliders <- renderUI({
-    group_number <- as.integer(input$groups)
-    lapply(1:group_number, function(i) {
-      sliderInput(inputId = paste0("group_slider",i), label = paste("Group",i),
-                  min = 1, max = RowNumbers(), value = c(1,RowNumbers()),step = 1)
-    })
-  })
-  GroupsRanges <- reactive({
-    group_number <- as.integer(input$groups)
-    mins <- sapply(1:group_number, function(i){
-      as.numeric(input[[paste0("group_slider",i)]])[1]
-    })
-    maxs <- sapply(1:group_number, function(i){
-      as.numeric(input[[paste0("group_slider",i)]])[2]
-    })
-    group_ranges <- as_data_frame(cbind(mins,maxs))
-    group_ranges
-  })
-  deltaCtTableSubset <- reactive({
-    df <- deltaCtTable()
-    if (is.null(df)){
-      return(NULL)
-    }
-    groups_df <- GroupsRanges()
-    min <- 1
-    max <- nrow(df)
-    final_df <- df[min:max,]
-    if(nrow(groups_df) > 1){
-      min <- groups_df[1,1] 
-      max <- groups_df[1,2] 
-      final_df <- df[min:max,]
-      for ( i in 2:nrow(groups_df)){
-        min <- groups_df[i,1] # min
-        max <- groups_df[i,2] # max
-        subset_df <- df[min:max,]
-        subset_df$Sample <- rep(paste0(subset_df$Sample[1],"_",i), times = length(subset_df$Sample))
-        final_df <- rbind(final_df, subset_df)
-      }
-    }
-    final_df
-  })
-  # This function is used to render the Plot
-  output$plot1 <- renderPlot({
+  ## This function is used to render the Plot
+  output$plot_1 <- renderPlot({
     plotInput()
-  },width = 400)
+  },width =  400 )
   # This function proccess and plot the data
   plotInput <- reactive({
     threshold = Threshold()
     threshold <- as.numeric(threshold)
-    colorblind_pallete =  c("#40b8d0", "#b2d183")
-    table <- deltaCtTableSubset()
+    colorblind_pallete <- c("#999999", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
+    table <- deltaCtTable()
     if (is.null(table)){
-      table <- deltaCtTable()
+      return(NULL)
     }
-    names(table) <- c("Sample", "Number", "value")
+    metadata_df <- metadata_table()
+    if (is.null(metadata_df)){
+      return(NULL)
+    } else {
+      metadata_df <- metadata_df %>%
+        dplyr::rename(sample_ID = Sample)
+      table <- table %>%
+        left_join(metadata_df, by = "sample_ID") 
+    }
+     
+    
+    names(table)[1:3] <- c("Sample", "Number", "value")
     ## Se os dados estiverem em log
     if(TRUE %in% (table$value < 0)){
       table$value <- sapply(table$value, function(x){10**x})
@@ -264,27 +241,58 @@ server <- function(input, output, session) {
     
     max_normal = 100
     table_positive <- subset(table, value > threshold)
-    dilutions <- unique(table_positive[,1])
+    table_negative <- subset(table, value <= threshold)
     
-    fig1 = ggplot(data=table,aes(x=Sample,y=value)) + 
-      stat_boxplot(data=subset(table, value > threshold),aes(x=Sample,y=value), geom = "errorbar", width = 0.7, alpha = 0.4 ) +
-      geom_boxplot(data =subset(table, value > threshold) , notch = TRUE, outlier.size = 0, notchwidth = 0.6, alpha = 0.3, colour = "black",aes(x=Sample ,y=value) ) +
-      geom_beeswarm(size=1.3, cex=1.5, priority='ascending', aes(color = Positive )) +
-      scale_y_log10(breaks = scales::trans_breaks("log10",function(x) 10^x),
-                    labels= scales::trans_format("log10", scales::math_format(10^.x))) +
-      theme_bw(base_size=16) + theme(axis.text.x = element_text(angle=45,hjust=1), legend.title = element_blank()) +
-      xlab("Sample") + ylab("RNA levels") +
-      scale_colour_manual(values = ifelse( table$value > threshold, colorblind_pallete[1], colorblind_pallete[2] ) )
-    fig1
+    #dilutions <- unique(table_positive[,1])
+    
+    #fig1 = ggplot(data=table,aes(x=Sample,y=value)) + 
+    #  stat_boxplot(data=subset(table, value > threshold),aes(x=Sample,y=value), geom = "errorbar", width = 0.7, alpha = 0.4 ) +
+    #  geom_boxplot(data =subset(table, value > threshold) , notch = TRUE, outlier.size = 0, notchwidth = 0.6, alpha = 0.3, colour = "black",aes(x=Sample ,y=value) ) +
+    #  geom_beeswarm(size=1.3, cex=1.5, priority='ascending', aes(color = Positive )) +
+    #  scale_y_log10(breaks = scales::trans_breaks("log10",function(x) 10^x),
+    #                labels= scales::trans_format("log10", scales::math_format(10^.x))) +
+    #  theme_bw(base_size=16) + theme(axis.text.x = element_text(angle=45,hjust=1), legend.title = element_blank()) +
+    #  xlab("Sample") + ylab("RNA levels") +
+    #  scale_colour_manual(values = ifelse( table$value > threshold, colorblind_pallete[1], colorblind_pallete[2] ) )
+    
+    fig_1 <- ggpubr::ggboxplot(data = table_positive, x = "Sample", y = "value",
+                               color = "Group", add = "jitter",
+                               #shape = "Group",
+                               notch = input$plot_1_notch,
+                               palette = "npg" , ## npg -> nature publishing group
+                               yscale = "log10")
+    
+   # groups_vector <- unique(table$Group)
+   # if (length(groups_vector) > 1) {
+   #   my_comparisons <- list( c(groups_vector[1], groups_vector[2]))
+   #   if (length(groups_vector) > 2){
+   #     for (i in 3:length(groups_vector)) {
+   #       my_comparisons[[i-1]] <- c(groups_vector[1],groups_vector[i])
+   #     }
+   #   }
+   #   fig_1 + stat_compare_means(comparisons = my_comparisons) # Add pairwise comparisons p-value
+   #     #stat_compare_means()    # label.y = 50               # Add global p-value
+   # }
+    fig_1
+    
   })
   
   ## This function is used to save the plot as a PDF file
-  output$downloadPlot <- downloadHandler(
+  output$download_plot_1 <- downloadHandler(
     filename = function() { paste(input$gene1, '.pdf', sep='') },
     content = function(file) {
       ggsave(file, plot = plotInput(), device = "pdf")
     }
   )
+  
+  ## Function to download deltaCT excel table
+  output$download_deltaCT_table <- downloadHandler(
+    filename = function() { "delta_Ct_table.xlsx" },
+    content = function(file) {
+      tempFile <- tempfile(fileext = ".xlsx")
+      xlsx::write.xlsx(deltaCtTable(), tempFile)
+      file.rename(tempFile, file)
+    })
 }
 
 server
--- ui.R
+++ ui.R
@@ -1,10 +1,5 @@
 #
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-# 
-#    http://shiny.rstudio.com/
+# This is the user-interface definition of a Shiny web application.
 #
 
 library(shiny)
@@ -20,10 +15,9 @@ ui <- fluidPage(
       fileInput('file2', 'If needed, choose another File',
                 accept=c('.xls','.xlsx')),
       tags$hr(),
-      # radioButtons('design', 'Design',
-      #              c(Duplicate='Duplicate',
-      #                Triplicate='Triplicate'),
-      #              'Duplicate'),
+      fileInput('file_metadata', 'Choose a file for experiment design',
+                accept=c('.xls','.xlsx')),
+      tags$hr(),
       selectInput(inputId = "gene1", label = "Select target gene:",
                   choices =  c(" "), selected = " "),
       sliderInput("range1", "Tm range to accept for target gene",min = 50, max = 90, value = c(50,90),
@@ -34,22 +28,16 @@ ui <- fluidPage(
                   step = 0.5, width = "90%"),
       textInput(inputId = "threshold", label = "deltaCT detection threshold", 
                 value = 1e-7 ),
-      selectInput("groups", "Number of groups",
-                  list("1", "2", "3", "4", "5","6","7","8","9","10"), selected = "1"),
-      conditionalPanel(
-        condition = "input.groups != '1'",
-        uiOutput("group_sliders")
-      ),
-      downloadButton('downloadPlot')
+      submitButton("Apply Changes")
     ),
     mainPanel(
       tabsetPanel(
-        #column(6,plotOutput(outputId="plotgraph1", width="300px",height="300px")),  
-        #column(6,plotOutput(outputId="plotgraph2", width="300px",height="300px"))
         tabPanel('CT',column(5, h3("Target Gene"),tableOutput('CT1')),
                  column(5, h3("Reference Gene"),tableOutput('CT2'))),
-        tabPanel('deltaCT',tableOutput('deltaCT')),
-        tabPanel('Plots', plotOutput('plot1'))
+        tabPanel('deltaCT',tableOutput('deltaCT'),
+                 downloadButton("download_deltaCT_table","Download Excel File")),
+        tabPanel('Plots',  checkboxInput("plot_1_notch", "Apply notch", FALSE),plotOutput('plot_1'),
+                 downloadButton('download_plot_1',"Download Figure"))
       )
     )
   )
